830823f4fe007b1e33f8c0b3f4fcb92afdffa8df
tasthing <lufarionite@gmail.com>
2020-01-21 16:59:11 -0500

    Fix some things

--- Assets/templatescript.cs
+++ Assets/templatescript.cs
@@ -8,16 +8,17 @@ using rnd = UnityEngine.Random;
 
 public class templatescript : MonoBehaviour
 {
-    public KMAudio Audio;
+    public new KMAudio audio;
     public KMBombInfo bomb;
+	public KMBombModule module;
 
-    static int moduleIdCounter = 1;
-    int moduleId;
+    private static int moduleIdCounter = 1;
+    private int moduleId;
     private bool moduleSolved;
 
     void Awake()
     {
-      moduleId = moduleIdCounter++;
+    	moduleId = moduleIdCounter++;
     }
 
     void Start()
